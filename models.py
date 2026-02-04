3c4bba2cbfcb54850a9dfc9a005987336fd21cea
gpetuhov201-lgtm <gpetuhov201@gmail.com>
2026-02-04 20:56:52 +0300

    create class Enemy,reate enemies,create counter

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from pygame import *
+from random import *
 
 class Character(sprite.Sprite):
     def __init__(self,x,y,outfit,w,h,speed,window):
@@ -13,9 +14,22 @@ class Character(sprite.Sprite):
         self.window.blit(self.image,(self.rect.x, self.rect.y))
 
 class Player(Character):
+    counter = 0
     def move(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_LEFT] and self.rect.x > 0:
             self.rect.x -= self.speed
         if keys_pressed[K_RIGHT] and self.rect.x < self.window.get_size()[0] - self.rect.width:
             self.rect.x += self.speed
+
+class Enemy(Character):
+    def update(self):
+        self.draw()
+        if self.rect.y < self.window.get_size()[1] - self.rect.height:
+            self.rect.y += self.speed
+        else:
+            self.rect.y = randint(-200,0)
+            self.player.counter += 1
+            self.speed = randint(1,3)
+    def count(self,player):
+        self.player = player
--- shooter_game.py
+++ shooter_game.py
@@ -1,22 +1,45 @@
 from pygame import *
 from models import *
+from random import *
 
 window = display.set_mode((700,500))
 background = transform.scale(image.load('media/images/galaxy.jpg'),window.get_size())
 window.blit(background,(0,0))
 
-player: Player = Player(250,400,'media/images/rocket.png',75,100,1,window)
+font.init()
+text_font = font.Font(None,60)
+x = 0
+y = 0
+x_step = window.get_size()[0] // 3
+y_step = -50
+enemies = sprite.Group()
+player: Player = Player(250,420,'media/images/rocket.png',60,85,4,window)
+for i in range(9):
+    cord_x = randint(x,x + x_step)
+    enemy = Enemy(cord_x,y,'media/images/ufo.png',50,50,randint(1,3),window)
+    print(cord_x,y,x_step)
+    x += x_step
+    if i % 3 == 0:
+        y += y_step
+        x = 0
+    enemies.add(enemy)
+    enemy.count(player)
+print(len(enemies))
 
 
 
 flag = True
 clock = time.Clock()
+FPS = 60
 while flag:
     window.blit(background,(0,0))
     player.draw()
     player.move()
+    enemies.update()
+    text = text_font.render('Пропущено:' + str(player.counter),True,(235, 52, 52))
+    window.blit(text,(50,50))
     display.update()
-    clock.tick()
+    clock.tick(FPS)
     events = event.get()
     for i in events:
         if i.type == QUIT:
